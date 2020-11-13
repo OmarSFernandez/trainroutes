daf2cef1ce216983753bf0e4df7c6c65258d786d
Omar Fernandez <ofernandez@users.noreply.github.com>
2020-11-12 21:27:42 -0500

    Completed question 1

--- trainroutes.py
+++ trainroutes.py
@@ -2,19 +2,23 @@ import requests     # Module for accessing website data
 import json         # Module for parsing JSON resulting from request for data
 
 #Setting variables for authentication
-''' mbtaSite = 'https://mbta.com/' '''
 mbtaSite = 'https://api-v3.mbta.com/routes?filter[type]=0,1'    #accessing only the light (type:0) and heavy (type:1) rail types
 userName = 'omar.str@gmail.com'
 key = '2fbf2f24d7af4315a1e999a89c54811c'
 
-print('Here are the long names for all Light Rail and Heavy Rail subway lines:')
 # Using requests module to access the MBTA website.
 # Using API key to prevent getting rate-limited.
 reqResults = requests.get(mbtaSite, auth=(userName, key))
 
 #Letting the user know that accessing the website was successful (2xx = success)
 print('The status code is: ' + str(reqResults.status_code))
-resDict = json.dumps(reqResults.text)
-print('The following is the dictionary information from the request: ')
-print(resDict)
+resDict = json.loads(reqResults.text)
 
+#Accessing the value of the main key 'data'
+data_value = resDict['data']
+
+print('Here are the long names for all Light Rail and Heavy Rail subway lines:')
+for attributeData in data_value:
+    myAttribute = attributeData['attributes']
+    longName = myAttribute['long_name']
+    print(longName)
