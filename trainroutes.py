3495e95ad85af92ea9a03c848166670168d98fde
Omar Fernandez <ofernandez@users.noreply.github.com>
2020-11-14 01:11:18 -0500

    Added function to extract data from api call

--- trainroutes.py
+++ trainroutes.py
@@ -1,24 +1,34 @@
 import requests     # Module for accessing website data
 import json         # Module for parsing JSON resulting from request for data
+import re           # Module for regular expressions 
 
 #Setting variables for authentication
-mbtaSite = 'https://api-v3.mbta.com/routes?filter[type]=0,1'    #accessing only the light (type:0) and heavy (type:1) rail types
-userName = 'omar.str@gmail.com'
+api_url_1 = 'https://api-v3.mbta.com/routes?filter[type]=0,1'    #accessing only the light (type:0) and heavy (type:1) rail types
+user_name = 'omar.str@gmail.com'
 key = '2fbf2f24d7af4315a1e999a89c54811c'
 
-# Using requests module to access the MBTA website.
-# Using API key to prevent getting rate-limited.
-reqResults = requests.get(mbtaSite, auth=(userName, key))
+#Function that returns
+def data_extract(api_url):
+    # Using requests module to access the MBTA website.
+    # Using API key to prevent getting rate-limited.
+    req_response = requests.get(api_url, auth=(user_name, key))   #Returns a json string
+    #Letting the user know that accessing the website was successful (2xx = success)
+    print('The status code is: ' + str(req_response.status_code))
+    #Converting json string to dictionary for data extraction
+    req_dict = json.loads(req_response.text)          
+    #Accessing the value of the main key 'data', which is a list of dictionaries
+    data_value = req_dict['data']
+    #Return the list of dictionaries containing the data
+    return data_value
 
-#Letting the user know that accessing the website was successful (2xx = success)
-print('The status code is: ' + str(reqResults.status_code))
-resDict = json.loads(reqResults.text)
+data_value_1 = data_extract(api_url_1)
 
-#Accessing the value of the main key 'data'
-data_value = resDict['data']
+#Answering part 1
+print('1. Long names for all Light Rail and Heavy Rail subway lines:')
+for listed_attr in data_value_1:                #Iterating through each dictionary in the data list
+    current_attr = listed_attr['attributes']   #Extracting value for 'attribute' key
+    long_name = current_attr['long_name']         #Extracting value of 'long_name' key
+    print(long_name)
 
-print('Here are the long names for all Light Rail and Heavy Rail subway lines:')
-for attributeData in data_value:
-    myAttribute = attributeData['attributes']
-    longName = myAttribute['long_name']
-    print(longName)
+#Answering part 2 a and b
+#data_value_2 = data_extract(api_url_2)
