bd6af9826d212c9f19abcac5d80d2ae70f32a6e1
Omar Fernandez <ofernandez@users.noreply.github.com>
2020-11-18 01:58:28 -0500

    Part 2a and b done. Want to clean up how I print 2c.

--- trainroutes.py
+++ trainroutes.py
@@ -10,7 +10,7 @@ api_url_2 = 'https://api-v3.mbta.com/stops?filter[route_type]=0,1&include=route'
 user_name = 'omar.str@gmail.com'
 key = '2fbf2f24d7af4315a1e999a89c54811c'
 
-#Function that returns
+#Function that returns list of dictionaries containing
 def data_extract(api_url):
     # Using requests module to access the MBTA website.
     # Using API key to prevent getting rate-limited.
@@ -68,15 +68,15 @@ two_dict = Counter(route_list)                       # Importing list of routes 
 
 
 
-print('Here is the counter dictionary representing each route and the number of stops they represent')
-print(two_dict)
+'''print('Here is the counter dictionary representing each route and the number of stops they represent')
+print(two_dict)'''
 
 #Extracting the routes with the least and most stops from the counter dictionary
-minimum = min(two_dict, key=two_dict.get)           
+'''minimum = min(two_dict, key=two_dict.get)           
 maximum = max(two_dict, key=two_dict.get)
 
 print('Route with the least stops: ' + str(minimum) + ' with ' + str(two_dict[minimum]) + ' stops')
-print('Route with the most stops: ' + str(maximum) + ' with ' + str(two_dict[maximum]) + ' stops')
+print('Route with the most stops: ' + str(maximum) + ' with ' + str(two_dict[maximum]) + ' stops')'''
 
 #Answering 2c
 
@@ -84,9 +84,10 @@ print('Route with the most stops: ' + str(maximum) + ' with ' + str(two_dict[max
 for line in two_dict.keys():
     route_dict.add(line, [])
 
-print(sorted_data_list)
-print(route_dict)
+'''print(sorted_data_list)
+print(route_dict)'''
 
+#Eliminating repeated stop entries for alternate direction on each line.
 for entry in sorted_data_list:
     if any(entry['stop'] in s for s in route_dict[entry['line']]):
         continue
@@ -94,3 +95,47 @@ for entry in sorted_data_list:
         route_dict[entry['line']] += [entry['stop']]
 
 pprint(route_dict)
+
+#Let's count the number of stops on each line!
+count_dict = {}
+for line in route_dict:
+    stop_count = len(route_dict[line])
+    count_dict[line] = stop_count
+    print("The number of stops on " + line + " is " + str(stop_count))
+
+'''print(count_dict)'''
+
+min_stops = min(count_dict.values()) 
+max_stops = max(count_dict.values()) 
+min_res = [key for key in count_dict if count_dict[key] == min_stops] 
+max_res = [key for key in count_dict if count_dict[key] == max_stops]
+
+print("The line(s) with the least stops is(are): " +  str(min_res) + " with " + str(min_stops) + " stops")
+print("The line(s) with the most stops is(are): " +  str(max_res) + " with " + str(max_stops) + " stops")
+
+connection_dict = my_dictionary() 
+# Algirithm to compare every lines list of stops to determine if they have stops in common. 
+# Creating dictionary with common stops as keys and lists of intersecting lines as values.
+for prime_key in route_dict:                
+    cur_list = route_dict[prime_key]
+    for sec_key in route_dict:                                          # Compare every list of stops to every list of stops minus itself
+        if prime_key == sec_key:
+            break
+        elif bool(set(cur_list) & set(route_dict[sec_key])) == False:   #
+            continue
+        else: 
+            connections = str(set(cur_list) & set(route_dict[sec_key]))  # Returns set stops that appear in both lists
+            for items in connections:                                    # Iterating through matching tops. Checking if common stops have keys in the connection dictionary
+                if items in connection_dict.keys():
+                    continue
+                else:
+                    connection_dict.add(items, [])
+                    if any(prime_key in s for s in connection_dict[items]) == False:
+                        connection_dict[items] += [prime_key]
+                    if any(sec_key in s for s in connection_dict[items]) == False:
+                        connection_dict[items] += [sec_key]
+            
+            print("Stop(s): " + connections + " connects: " + prime_key + " and " + sec_key)
+
+
+pprint(connection_dict)
